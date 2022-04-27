a86e1fac4c1e6a77342ab9e01af0e9844eae9030
Thakshila Nuwangi <thakshilanuwangi123@gmail.com>
2022-04-27 15:09:23 +0530

    finalize search records

--- src/main/java/lk/ijse/dep8/library/api/IssueServlet.java
+++ src/main/java/lk/ijse/dep8/library/api/IssueServlet.java
@@ -3,97 +3,149 @@ package lk.ijse.dep8.library.api;
 import jakarta.json.bind.Jsonb;
 import jakarta.json.bind.JsonbBuilder;
 import jakarta.json.bind.JsonbException;
+import lk.ijse.dep8.library.dto.BookDTO;
 import lk.ijse.dep8.library.dto.IssueDTO;
+import lk.ijse.dep8.library.exception.ValidationException;
 
 import javax.annotation.Resource;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
 import javax.sql.DataSource;
-import javax.xml.bind.ValidationException;
 import java.io.IOException;
 import java.sql.*;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "IssueServlet", value = {"/issues", "/issues/"})
 public class IssueServlet extends HttpServlet {
 
     @Resource(name = "java:comp/env/jdbc/pool4library")
-    private DataSource pool;
+    private volatile DataSource pool;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String query = req.getParameter("q");
+        query = "%" + ((query == null) ? "" : query) + "%";
+
+        try (Connection connection = pool.getConnection()) {
+
+            boolean pagination = req.getParameter("page") != null &&
+                    req.getParameter("size") != null;
+            String sql = "SELECT * FROM issue WHERE isbn LIKE ? OR nic LIKE ? OR date LIKE ? " + ((pagination) ? "LIMIT ? OFFSET ?" : "");
+
+            PreparedStatement stm = connection.prepareStatement(sql);
+            PreparedStatement stmCount = connection.prepareStatement("SELECT count(*) FROM issue WHERE isbn LIKE ? OR nic LIKE ? OR date LIKE ?");
+
+            stm.setString(1, query);
+            stm.setString(2, query);
+            stm.setString(3, query);
+            stmCount.setString(1, query);
+            stmCount.setString(2, query);
+            stmCount.setString(3, query);
+
+            if (pagination) {
+                int page = Integer.parseInt(req.getParameter("page"));
+                int size = Integer.parseInt(req.getParameter("size"));
+                stm.setInt(4, size);
+                stm.setInt(5, (page - 1) * size);
+            }
+            ResultSet rst = stm.executeQuery();
+
+            List<IssueDTO> issues = new ArrayList<>();
+
+            while (rst.next()) {
+                issues.add((new IssueDTO(
+                        rst.getInt("id"),
+                        rst.getString("nic"),
+                        rst.getString("isbn"),
+                        rst.getDate("date")
+                )));
+            }
+
+            ResultSet rst2 = stmCount.executeQuery();
+            if (rst2.next()) {
+                resp.setHeader("X-Count", rst2.getString(1));
+            }
+
+            resp.setContentType("application/json");
+            Jsonb jsonb = JsonbBuilder.create();
+            jsonb.toJson(issues, resp.getWriter());
+
+        } catch (SQLException t) {
+            t.printStackTrace();
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+
+    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        if (req.getContentType() == null || !req.getContentType().startsWith("application/json")) {
+
+        /* Let's validate the request first */
+
+        /* Validating the content type */
+        if (req.getContentType() == null || !req.getContentType().startsWith("application/json")){
             resp.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE);
             return;
         }
 
-        try {
+        try{
+            /* Let's try to convert the JSON into Java Object (IssueDTO) */
             Jsonb jsonb = JsonbBuilder.create();
             IssueDTO issue = jsonb.fromJson(req.getReader(), IssueDTO.class);
 
-            if (issue.getNic() == null || !issue.getNic().matches("\\d{9}[Vv]")) {
+            /* Let's validate the nic and isbn */
+            if (issue.getNic() == null || !issue.getNic().matches("\\d{9}[Vv]")){
                 throw new ValidationException("Invalid NIC");
-            } else if (issue.getIsbn() == null || !issue.getIsbn().matches("\\d+")) {
+            }else if (issue.getIsbn() == null || !issue.getIsbn().matches("\\d+")){
                 throw new ValidationException("Invalid ISBN");
             }
 
             try (Connection connection = pool.getConnection()) {
-                /*PreparedStatement stm = connection.prepareStatement("SELECT * FROM member WHERE nic=?");
-                stm.setString(1, issue.getNic());
-                if (!stm.executeQuery().next()) {
-                    resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid NIC");
-                    return;
-                }
 
-                stm = connection.prepareStatement("SELECT * FROM book WHERE isbn=?");
-                stm.setString(1, issue.getIsbn());
-                if (!stm.executeQuery().next()) {
-                    resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid ISBN");
-                    return;
-                }*/
-
-                PreparedStatement stm = connection.prepareStatement("SELECT * FROM member INNER JOIN book WHERE nic=? AND isbn=?");
+                /* Let's check whether the nic and isbn are within the DB */
+                PreparedStatement stm = connection.prepareStatement("SELECT * FROM book INNER JOIN member WHERE nic=? AND isbn=?");
                 stm.setString(1, issue.getNic());
                 stm.setString(2, issue.getIsbn());
-                if (!stm.executeQuery().next()) {
-                    resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid NIC or Invalid ISBN");
-                    return;
+                if (!stm.executeQuery().next()){
+                    throw new ValidationException("Invalid NIC or Invalid ISBN");
                 }
 
-                stm = connection.prepareStatement("SELECT * FROM issue WHERE isbn=?");
+                /* Let's check the book availability */
+                stm = connection.prepareStatement("SELECT * FROM issue WHERE isbn = ?");
                 stm.setString(1, issue.getIsbn());
-                ResultSet rst = stm.executeQuery();
-                if (rst.next()) {
-                    resp.sendError(HttpServletResponse.SC_GONE, "The book has been already issued");
+                if (stm.executeQuery().next()){
+                    resp.sendError(HttpServletResponse.SC_GONE, "Book is not available");
                     return;
                 }
 
-                /*DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
-                LocalDateTime now = LocalDateTime.now();*/
-
+                /* Let's place the issue */
                 issue.setDate(Date.valueOf(LocalDate.now()));
-
-                stm = connection.prepareStatement("INSERT INTO issue (nic, isbn, date) VALUES (?,?,?)");
+                stm = connection.prepareStatement("INSERT INTO issue (nic, isbn, date) VALUES (?,?,?)", Statement.RETURN_GENERATED_KEYS);
                 stm.setString(1, issue.getNic());
                 stm.setString(2, issue.getIsbn());
                 stm.setDate(3, issue.getDate());
-                if (stm.executeUpdate() != 1) {
-                    throw new RuntimeException("Failed to record the issue details");
+                if (stm.executeUpdate() != 1){
+                    resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Failed to place the issue");
+                    return;
                 }
-                resp.setStatus(HttpServletResponse.SC_OK);
-            } catch (SQLException e) {
-                e.printStackTrace();
-                resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                ResultSet generatedKeys = stm.getGeneratedKeys();
+                generatedKeys.next();
+                issue.setId(generatedKeys.getInt(1));
+
+                resp.setContentType("application/json");
+                resp.setStatus(HttpServletResponse.SC_CREATED);
+                jsonb.toJson(issue, resp.getWriter());
             }
-        } catch (JsonbException e) {
+        }catch(JsonbException e) {
             resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid JSON");
-        } catch (ValidationException e) {
-            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }catch (ValidationException e){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
+        }catch (Throwable t){
+            t.printStackTrace();
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
     }
 }
